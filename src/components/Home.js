11a6fbe90b7b0dad5bf3ad203777b382c850bd3a
Bhawna <singhbhawna0603@gmail.com>
2022-03-02 22:14:58 +0530

    slider added

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "./Navbar";
+import Slider from "./Slider";
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Slider/>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
